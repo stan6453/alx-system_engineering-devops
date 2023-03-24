d9f7b64ca078e84f7d30af42cae37b1881d7ad85
stanley <stanley6453@gmail.com>
2023-03-24 20:30:08 +0100

    fixed puppet manifest syntax mistakes

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 #kills a process named killmenow
 exec { 'pkill killmenow':
-  command => 'pkill killmenow',
-  onlyif  => 'pgrep killmenow',
+  command  => 'pkill killmenow',
+  onlyif   => 'pgrep killmenow',
+  provider => 'shell',
 }
