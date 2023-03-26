c0bbd7aacc95742e8110687642da90ce256a257d
stanley <stanley6453@gmail.com>
2023-03-26 07:22:55 +0100

    created file 0x0B-ssh/100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # make changes to our configuration file. Set up your client SSH configuration
 file_line { 'IdentityFile':
     ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
+    path   => '~/.ssh/config',
     line   => 'IdentityFile    ~/.ssh/school',
 }
 
 file_line { 'Disable PasswordAuthentication':
     ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
+    path   => '~/.ssh/config',
     line   => 'PasswordAuthentication no',
 }
