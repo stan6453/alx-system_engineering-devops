dfc4b8d0095cea2ddf75874f9532d28cfeff6c29
stanley <stanley6453@gmail.com>
2023-03-26 05:29:54 +0100

    created file 0x0B-ssh/100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
-file { '~/.ssh/config'
-    path    => '~/.ssh/config'
+file { '~/.ssh/config':
+    path    => '~/.ssh/config',
     content => '
     Host    alxserver
         Hostname        52.201.220.51
         User    ubuntu
         IdentityFile    ~/.ssh/school
-    '
+    ',
 }
