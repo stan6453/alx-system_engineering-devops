01b0dd71dbbafd6019116b2da574058c8967ba61
stanley <stanley6453@gmail.com>
2023-03-26 05:57:41 +0100

    created file 0x0B-ssh/100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -10,11 +10,11 @@ ssh_authorized_key { 'school':
     name   => 'school',
     type   => 'ssh-rsa',
     key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDa6FbFqmpM1uEFrMPHdw7ooPvbvf3Ez+YqxXiIW+gabfDoUqiJ1IR5wCI\
-o+XMmJwjk+Yr+VF6XQGjN3ECzhU0yy3VXxdUKKqYO4AycdNC6aynJFrKlGoAOPbx5Tn4Qz763QuDHDDhGmheQL4zluen2IP1W52NSD+rWPuRbag\
-B586eYRQJo+5bab27o3/wROTtRsCQa+VzqSbKhKB6QOlgdn3Z3ztwObSYyQcPv6jGQjwldQB2KwsMI7Z3VcNARIVy4lkSdJKJ2l6Ooim/19bif9\
-HfXhkXdcN4wYM0CqG1KazX3RW7rIlLsbjE9WWzZQ/ZYHVqIZYinEQxMeqL/harL9ouDR6XPhbojL2I1b8up311t2vTmW7t1OgfRXSD+DqZ5sxIh\
-KxsfGbKkeJtXsDnyZMWlvwbxQIcsoy33KFFgZdxhAeLsHNTZoeQjWtMH4EV7ASWNuwT1t9JTlq4B4rhdLRWaKsYYP7ktI1c4zcGs8vaIvq1nYcW\
-08DvVYzarJI8= stanley@kali',
+    o+XMmJwjk+Yr+VF6XQGjN3ECzhU0yy3VXxdUKKqYO4AycdNC6aynJFrKlGoAOPbx5Tn4Qz763QuDHDDhGmheQL4zluen2IP1W52NSD+rWPuRbag\
+    B586eYRQJo+5bab27o3/wROTtRsCQa+VzqSbKhKB6QOlgdn3Z3ztwObSYyQcPv6jGQjwldQB2KwsMI7Z3VcNARIVy4lkSdJKJ2l6Ooim/19bif9\
+    HfXhkXdcN4wYM0CqG1KazX3RW7rIlLsbjE9WWzZQ/ZYHVqIZYinEQxMeqL/harL9ouDR6XPhbojL2I1b8up311t2vTmW7t1OgfRXSD+DqZ5sxIh\
+    KxsfGbKkeJtXsDnyZMWlvwbxQIcsoy33KFFgZdxhAeLsHNTZoeQjWtMH4EV7ASWNuwT1t9JTlq4B4rhdLRWaKsYYP7ktI1c4zcGs8vaIvq1nYcW\
+    08DvVYzarJI8= stanley@kali',
   }
 
 file { '~/.ssh/config':
