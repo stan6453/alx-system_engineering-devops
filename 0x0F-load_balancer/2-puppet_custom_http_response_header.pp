d6d023226bc44db2773792c3c393ff9450e00015
stanley <stanley6453@gmail.com>
2023-04-05 09:17:56 +0100

    added file 0x0F-load_balancer/2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,68 +4,67 @@
 #   using curl, it must return a page that contains the string Hello World!
 # - The redirection must be a “301 Moved Permanently”
 
-exec { 'apt-get update':
-  command => 'apt-get update',
-  path    => ['/usr/bin', '/usr/sbin'],
-}
+# exec { 'apt-get update':
+#   command => 'apt-get update',
+#   path    => ['/usr/bin', '/usr/sbin'],
+# }
 
-exec { 'apt-get install nginx':
-  command => 'apt-get install nginx -y',
-  path    => ['/usr/bin', '/usr/sbin'],
-}
+# exec { 'apt-get install nginx':
+#   command => 'apt-get install nginx -y',
+#   path    => ['/usr/bin', '/usr/sbin'],
+# }
 
-package { 'nginx':
-  ensure   => 'installed',
-  name     => 'nginx',
-  provider => 'apt',
-}
+# package { 'nginx':
+#   ensure   => 'installed',
+#   name     => 'nginx',
+#   provider => 'apt',
+# }
 
-file { '/var/www/html/index.nginx-debian.html':
-  ensure  => 'present',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'Hello World!',
-  mode    => '0744',
-}
+# file { '/var/www/html/index.nginx-debian.html':
+#   ensure  => 'present',
+#   owner   => 'www-data',
+#   group   => 'www-data',
+#   content => 'Hello World!',
+#   mode    => '0744',
+# }
 
-file { '/var/www/html/404.html':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => "Ceci n'est pas une page",
-}
+# file { '/var/www/html/404.html':
+#   mode    => '0744',
+#   owner   => 'www-data',
+#   group   => 'www-data',
+#   content => "Ceci n'est pas une page",
+# }
 
-file_line { 'add 301 redirection':
-  path  => '/etc/nginx/sites-available/default',
-  match => '# pass PHP scripts to FastCGI server',
-  line  => "
-	location /redirect_me {
-		return 301 https://quickref.me/bash;
-	}
-	# pass PHP scripts to FastCGI server
-	",
-}
+# file_line { 'add 301 redirection':
+#   path  => '/etc/nginx/sites-available/default',
+#   match => '# pass PHP scripts to FastCGI server',
+#   line  => "
+# 	location /redirect_me {
+# 		return 301 https://quickref.me/bash;
+# 	}
+# 	# pass PHP scripts to FastCGI server
+# 	",
+# }
 
-file_line { 'add 404 not found page':
-  path  => '/etc/nginx/sites-available/default',
-  match => '# pass PHP scripts to FastCGI server',
-  line  => "
-	error_page 404 /404.html;
-	location = /404.html {
-		internal;
-	}
-	# pass PHP scripts to FastCGI server
+# file_line { 'add 404 not found page':
+#   path  => '/etc/nginx/sites-available/default',
+#   match => '# pass PHP scripts to FastCGI server',
+#   line  => "
+# 	error_page 404 /404.html;
+# 	location = /404.html {
+# 		internal;
+# 	}
+# 	# pass PHP scripts to FastCGI server
 	
-	",
-}
+# 	",
+# }
 
 file_line { 'add  custom HTTP header must be X-Served-By':
   path  => '/etc/nginx/sites-available/default',
-  match => 'server {',
+  match => '^server {',
   line  => "
-  add_header X-Served-By $hostname;
-  server {
-  ",
+add_header X-Served-By \$hostname;
+server {",
 }
 
 exec { 'sudo service nginx restart':
