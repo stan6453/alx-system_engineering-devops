57a5bfc21d163bc2ae78e221d8ac0e98143ee988
stanley <stanley6453@gmail.com>
2023-04-05 08:39:53 +0100

    modified file alx-system_engineering-devops/0x0C-web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,36 +4,36 @@
 #   using curl, it must return a page that contains the string Hello World!
 # - The redirection must be a “301 Moved Permanently”
 
-# exec { 'apt-get update':
-#   command => 'apt-get update',
-#   path    => ['/usr/bin', '/usr/sbin'],
-# }
+exec { 'apt-get update':
+  command => 'apt-get update',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
 
-# exec { 'apt-get install nginx':
-#   command => 'apt-get install nginx -y',
-#   path    => ['/usr/bin', '/usr/sbin'],
-# }
+exec { 'apt-get install nginx':
+  command => 'apt-get install nginx -y',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
 
-# package { 'nginx':
-#   ensure   => 'installed',
-#   name     => 'nginx',
-#   provider => 'apt',
-# }
+package { 'nginx':
+  ensure   => 'installed',
+  name     => 'nginx',
+  provider => 'apt',
+}
 
-# file { '/var/www/html/index.nginx-debian.html':
-#   ensure  => 'present',
-#   owner   => 'www-data',
-#   group   => 'www-data',
-#   content => 'Hello World!',
-#   mode    => '0744',
-# }
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'Hello World!',
+  mode    => '0744',
+}
 
-# file { '/var/www/html/404.html':
-#   mode    => '0744',
-#   owner   => 'www-data',
-#   group   => 'www-data',
-#   content => "Ceci n'est pas une page",
-# }
+file { '/var/www/html/404.html':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "Ceci n'est pas une page",
+}
 
 file_line { 'add 301 redirection':
   path  => '/etc/nginx/sites-available/default',
@@ -59,16 +59,16 @@ file_line { 'add 404 not found page':
 	",
 }
 
-# exec { 'sudo service nginx restart':
-#   command => 'sudo service nginx restart',
-#   path    => ['/usr/bin', '/usr/sbin'],
-# }
+exec { 'sudo service nginx restart':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
 
-# service { 'nginx':
-#   ensure  => 'running',
-#   name    => 'nginx',
-#   enable  => 'true',
-#   start   => 'sudo service nginx start',
-#   stop    => 'sudo service nginx stop',
-#   restart => 'sudo service nginx restart',
-# }
+service { 'nginx':
+  ensure  => 'running',
+  name    => 'nginx',
+  enable  => 'true',
+  start   => 'sudo service nginx start',
+  stop    => 'sudo service nginx stop',
+  restart => 'sudo service nginx restart',
+}
