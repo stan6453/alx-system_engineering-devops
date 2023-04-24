3f995364a40b75302c2be3e395cce01b14ae1532
stanley <stanley6453@gmail.com>
2023-04-24 13:33:19 +0100

    fixed some errors in file 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,9 +14,14 @@ if __name__ == "__main__":
     user_todos = [todo for todo in all_todos if user['id'] == todo['userId']]
 
     with open('USER_ID.csv', 'w') as f:
-        f.write('"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"\n')
-        for todo in user_todos:
-            f.write('"{}","{}","{}","{}"\n'.format(
-                user['id'], user['username'],
-                todo['completed'], todo['title']
-            ))
+        for index, todo in enumerate(user_todos):
+            if index == len(user_todos)-1:
+                f.write('"{}","{}","{}","{}"'.format(
+                    user['id'], user['username'],
+                    todo['completed'], todo['title']
+                ))
+            else:
+                f.write('"{}","{}","{}","{}"\n'.format(
+                    user['id'], user['username'],
+                    todo['completed'], todo['title']
+                ))
