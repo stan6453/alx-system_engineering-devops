79782cc05afd667817d93cf766d6b7b4380f8187
stanley <stanley6453@gmail.com>
2023-04-24 13:19:40 +0100

    created file 0x15-api/2-export_to_JSON.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,24 +4,25 @@
 import requests
 import sys
 
-employee_id = sys.argv[1]
-user = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
-all_todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-user_todos = [todo for todo in all_todos if user['id'] == todo['userId']]
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
+    all_todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    user_todos = [todo for todo in all_todos if user['id'] == todo['userId']]
 
-number_of_tasks = 0
-for todo in user_todos:
-    number_of_tasks += 1
+    number_of_tasks = 0
+    for todo in user_todos:
+        number_of_tasks += 1
 
-number_of_task_done = 0
-for todo in user_todos:
-    if todo['completed'] == True:
-        number_of_task_done += 1
+    number_of_task_done = 0
+    for todo in user_todos:
+        if todo['completed'] == True:
+            number_of_task_done += 1
 
-print('Employee {} is done with tasks({}/{}):'
-      .format(user['name'], number_of_task_done, number_of_tasks)
-      )
-for todo in user_todos:
-    if todo['completed'] == True:
-        print('\t {}'.format(todo['title']))
+    print('Employee {} is done with tasks({}/{}):'
+        .format(user['name'], number_of_task_done, number_of_tasks)
+        )
+    for todo in user_todos:
+        if todo['completed'] == True:
+            print('\t {}'.format(todo['title']))
