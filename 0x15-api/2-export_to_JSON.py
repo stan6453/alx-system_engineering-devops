5882f767a7f8554135cd8f814cd03659a686a2f0
stanley <stanley6453@gmail.com>
2023-04-24 12:23:16 +0100

    created file 0x15-api/2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """query an API endpoint"""
 
-import requests
+import json
 from sys import argv
+import requests
 
 employee_id = argv[1]
 user = requests.get(
@@ -10,8 +11,16 @@ user = requests.get(
 all_todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
 user_todos = [todo for todo in all_todos if user['id'] == todo['userId']]
 
-with open('USER_ID.csv', 'w') as f:
-    """f.write('"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"\n')"""
-    for todo in user_todos:
-        f.write('"{}","{}","{}","{}"\n'
-                .format(user['id'], user['username'], todo['completed'], todo['title']))
+user_dict = {}
+user_dict[user['id']] = []
+
+for todo in user_todos:
+    todo['username'] = user['username']
+    todo['task'] = todo['title']
+    del todo['userId']
+    del todo['id']
+    del todo['title']
+    user_dict[user['id']].append(todo)
+
+with open('USER_ID.json', 'w') as file:
+    json.dump(user_dict,file)
