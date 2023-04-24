aa03b423c1c18a63692c9a7237f53a4b7511fba3
stanley <stanley6453@gmail.com>
2023-04-24 13:25:32 +0100

    fixed some errors in file 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,14 +4,21 @@
 import requests
 from sys import argv
 
-employee_id = argv[1]
-user = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
-all_todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-user_todos = [todo for todo in all_todos if user['id'] == todo['userId']]
+if __name__ == "__main__":
+    employee_id = argv[1]
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'
+        .format(employee_id)).json()
+    all_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+    user_todos = [todo for todo in all_todos if user['id'] == todo['userId']]
 
-with open('USER_ID.csv', 'w') as f:
-    """f.write('"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"\n')"""
-    for todo in user_todos:
-        f.write('"{}","{}","{}","{}"\n'
-                .format(user['id'], user['username'], todo['completed'], todo['title']))
+    with open('USER_ID.csv', 'w') as f:
+        """
+        f.write('"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"\n')
+        """
+        for todo in user_todos:
+            f.write('"{}","{}","{}","{}"\n'.format(
+                user['id'], user['username'],
+                todo['completed'], todo['title']
+            ))
