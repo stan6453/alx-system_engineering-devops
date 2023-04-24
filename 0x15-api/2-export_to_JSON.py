e609679b35258914132585a1a5347ecab3662821
stanley <stanley6453@gmail.com>
2023-04-24 13:43:59 +0100

    fixed some errors in file 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,25 +2,26 @@
 """query an API endpoint"""
 
 import json
-from sys import argv
 import requests
+from sys import argv
 
-employee_id = argv[1]
-user = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
-all_todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-user_todos = [todo for todo in all_todos if user['id'] == todo['userId']]
+if __name__ == "__main__":
+    employee_id = argv[1]
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)).json()
+    all_todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    user_todos = [todo for todo in all_todos if user['id'] == todo['userId']]
 
-user_dict = {}
-user_dict[user['id']] = []
+    user_dict = {}
+    user_dict[user['id']] = []
 
-for todo in user_todos:
-    todo['username'] = user['username']
-    todo['task'] = todo['title']
-    del todo['userId']
-    del todo['id']
-    del todo['title']
-    user_dict[user['id']].append(todo)
+    for todo in user_todos:
+        todo['username'] = user['username']
+        todo['task'] = todo['title']
+        del todo['userId']
+        del todo['id']
+        del todo['title']
+        user_dict[user['id']].append(todo)
 
-with open('{}.json'.format(user['id']), 'w') as file:
-    json.dump(user_dict,file)
+    with open('{}.json'.format(user['id']), 'w') as file:
+        json.dump(user_dict,file)
