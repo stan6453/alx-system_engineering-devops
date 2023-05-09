7da057f91e92e8c6d5b8b69cb7d52cc80196063e
stanley <stanley6453@gmail.com>
2023-05-09 13:57:23 +0100

    created 0x16-api_advanced/2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,7 +6,7 @@ for a given subreddit. """
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after='', count=0):
     """ recursive function that queries the Reddit API and
     returns a list containing the titles of all hot articles
     for a given subreddit. """
@@ -14,11 +14,24 @@ def recurse(subreddit, hot_list=[]):
     headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 12; SM-S906N\
                Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML,\
                like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile\
-               Safari/537.36'}
+               Safari/537.36'
+               }
+    parameters = {
+        'after': after,
+        'count': count,
+        'limit': 100
+    }
     try:
         data = requests.get(
-            'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit),
-            headers=headers, allow_redirects=False).json()
-        [print(post['data']['title']) for post in data['data']["children"]]
+            'https://www.reddit.com/r/{}/hot/.json'.format(subreddit),
+            headers=headers, params=parameters, allow_redirects=False).json()
+        hot_list.extend([post['data']['title']
+                        for post in data['data']["children"]])
+        if data['data']['after']:
+            return recurse(subreddit, hot_list,
+                           after=data['data']['after'],
+                           count=count+data['data']['dist'])
+        else:
+            return hot_list
     except Exception:
-        print('None')
+        return None
