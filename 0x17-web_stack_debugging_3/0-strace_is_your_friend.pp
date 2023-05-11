0e7a10d569a12783054c90ca076926d631930ee0
stanley <stanley6453@gmail.com>
2023-05-11 03:50:44 +0100

    added folder 0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # Fixed php wrong file extension in wp-settings.php
 exec { 'Fixed wrong file extension':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
