2420a8bee98720f8f1865a151d246575c7ce2f0a
stanley <stanley6453@gmail.com>
2023-05-18 17:02:45 +0100

    added folder 0x1B-web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# increase number of files nginx can handle simultaneously
+
+exec { 'increase ulimit':
+  command => 'sed -i "s/15/25000/" /etc/default/nginx',
+  path    => ['/usr/local/bin/', '/bin/'],
+}
+
+exec { 'restart-nginx':
+  command => 'service nginx restart',
+  path    => ['/etc/init.d/'],
+}
