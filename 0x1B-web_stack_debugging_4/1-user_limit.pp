cf83734cbd8a961430a1e57e1e88455858b5773e
stanley <stanley6453@gmail.com>
2023-05-18 17:58:20 +0100

    added file 0x1B-web_stack_debugging_4/1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Increase hard limit on holberton.
 exec { 'Increase hard limit':
-  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton hard/s/5/2000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 # Increase soft limit on holberton
 exec { 'Increase soft limit':
-  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton soft/s/4/1000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
