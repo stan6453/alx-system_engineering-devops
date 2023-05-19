57be6a467db290b41cbc812566c4898ae4ab56b2
stanley <stanley6453@gmail.com>
2023-05-19 12:14:14 +0100

    added file 0x1B-web_stack_debugging_4/1-user_limit.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # increase number of files nginx can handle simultaneously
 
 exec { 'increase ulimit':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command => 'sed -i "s/15/25000/" /etc/default/nginx',
   path    => ['/usr/local/bin/', '/bin/'],
 }
 
